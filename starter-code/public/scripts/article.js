09f7bb2a6a3bac6f1241f96d10a01b09e815b8b3
Kyle Richmond <kylerichmond79@gmail.com>
2017-07-24 12:00:27 -0700

    got as far as we could and pushing

--- starter-code/public/scripts/article.js
+++ starter-code/public/scripts/article.js
@@ -37,7 +37,7 @@ Article.prototype.toHtml = function() {
 Article.loadAll = rows => {
   rows.sort((a,b) => (new Date(b.publishedOn)) - (new Date(a.publishedOn)));
 
-  // TODO: Refactor this forEach code, by using a `.map` call instead, since what we are trying to accomplish
+  // DONE: Refactor this forEach code, by using a `.map` call instead, since what we are trying to accomplish
   // is the transformation of one collection into another. Remember that we can set variables equal to the result
   // of functions. So if we set a variable equal to the result of a .map, it will be our transformed array.
   // There is no need to push to anything.
@@ -47,7 +47,7 @@ Article.loadAll = rows => {
   Article.all.push(new Article(ele));
 });
 */
-
+  Article.all = Article.map(rawData => new Article(rawData));
 };
 
 Article.fetchAll = callback => {
@@ -62,14 +62,23 @@ Article.fetchAll = callback => {
 
 // TODO: Chain together a `map` and a `reduce` call to get a rough count of all words in all articles.
 Article.numWordsAll = () => {
-  return Article.all.map().reduce()
+  return Article.all.map(ele => {
+    ele.split(' ').map.length;
+  }).reduce((sum, value) => {
+    return sum + value;
+  }, 0);
 };
 
 // TODO: Chain together a `map` and a `reduce` call to produce an array of unique author names. You will
 // probably need to use the optional accumulator argument in your reduce call.
-Article.allAuthors = () => {
-  return Article.all.map().reduce();
-};
+Article.allAuthors = (ele => {return ele.author})
+  .reduce((acc, value) => {
+    if(!acc.includes(val)) {
+      acc.push(val);
+    }
+    return acc
+  }, []);
+
 
 Article.numWordsByAuthor = () => {
   return Article.allAuthors().map(author => {
@@ -80,7 +89,10 @@ Article.numWordsByAuthor = () => {
     // The first property should be pretty straightforward, but you will need to chain
     // some combination of filter, map, and/or reduce to get the value for the second
     // property.
-
+    return{
+    name: author,
+    words: ele.
+  }
   })
 };
 
